c522a87ca771bec8a62f0e02a8509299288af7d9
AloiSan <95715496+AloiSan@users.noreply.github.com>
2023-10-14 16:13:02 +0100

    first test for ResetDisplay

---
+++ UnitTestProject/TestsResetDisplay.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+using Hangman;
+
+namespace UnitTestProject
+{
+    public class TestsResetDisplay
+    {
+        [Fact]
+        public void TestResetDisplay()
+        {
+            GamePage gamepage = new("Easy");
+            string word = "testing";
+
+            gamepage.ResetDisplay(word);
+
+            for (int i = 0; i < 12; i++)
+            {
+                char letter = i < word.Length ? word[i] : ' ';
+                Label letterLabel = gamepage.FindByName<Label>("Letter" + (i + 1));
+                Assert.Equal(letter.ToString(), letterLabel.Text);
+            }
+
+        }
+    }
+}
